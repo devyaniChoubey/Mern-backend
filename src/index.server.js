2bf1178c979bb42f2488decf29f1d90e70cd3c3e
devyani choubey <devyanichoubey16@gmail.com>
2021-11-10 16:42:31 +0530

    fi

--- src/index.server.js
+++ src/index.server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const env = require('dotenv');
 const app = express();
 const mongoose = require('mongoose');
-
+const path = require('path');
 const authRoutes = require('./routes/auth')
 const adminRoutes = require('./routes/admin/auth');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
 const cartRoutes = require('./routes/cart');
+const cors = require('cors');
 
 env.config();
 
@@ -18,7 +19,9 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO
     console.log('Database connected')
 })
 
+app.use(cors());
 app.use(express.json());
+app.use('/public',express.static(path.join(__dirname, 'uploads')));
 app.use('/api', authRoutes);
 app.use('/api',adminRoutes);
 app.use('/api', categoryRoutes);
--- src/routes/category.js
+++ src/routes/category.js
@@ -3,9 +3,24 @@ const router = express.Router();
 const slugify = require('slugify');
 const { requireSignin, adminMiddleware } = require('../common-middleware');
 const { addCategory, getCategories } = require('../controller/category');
+const multer = require('multer');
+const shortid = require('shortid')
+const path = require('path')
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null,path.join(path.dirname(__dirname), 'uploads') )
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, shortid.generate() + '-' + file.originalname)
+    }
+})
+  
 
-router.post('/category/create',requireSignin,adminMiddleware, addCategory);
+const upload = multer({storage})
+
+router.post('/category/create',requireSignin,adminMiddleware,upload.single('categoryImage'), addCategory);
 router.get('/category/getcategory',getCategories);
 
 module.exports = router;
